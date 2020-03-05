5e6300aaa92c170b7a171cdcfd60f80aa482851c
9135ks@gr-kyoshin.biz <91335@kyoshin.local>
2020-03-05 12:12:35 +0900

    crad_fin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
